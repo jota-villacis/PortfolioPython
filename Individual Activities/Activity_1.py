94d1d4be2cc6094c8b943c208913f2896a3f93e1
Jimmy Villacis <68618212+jota-villacis@users.noreply.github.com>
2022-10-22 17:44:57 +1100

    Development activity number 1

---
+++ Individual Activities/Activity_1.py
@@ -0,0 +1,18 @@
+# Activity 1
+
+'''
+
+● Creation of a virtual environment.
+virtualenv -p python3.9 venv3.9
+
+● Identify the path of the folder where the virtual environment files are.
+pwd
+
+● Activation of the created virtual environment.
+. "projectDirectory"/bin/activate
+
+● Deactivation of the virtual environment
+deactivate
+
+
+'''
